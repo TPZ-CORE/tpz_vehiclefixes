1408ebf56bd3af63c626901c8d3a943090c749d9
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2026-09-20 16:00:49 +0300

    Refactor boat entity creation event handling

--- tpz_vehiclefixes/server/tp-server_boats.lua
+++ tpz_vehiclefixes/server/tp-server_boats.lua
@@ -1,15 +1,11 @@
-if Config.ActiveBoats then
-
+if Config.Boats.active then
     AddEventHandler('entityCreating', function(entity)
-
-        if entity and GetEntityType(entity) == 2 and GetVehicleType(entity) == "boat" then -- Check if its a vehicle and if this vehicle is a boat
-
-            if GetEntityPopulationType(entity) ~= 7 and GetEntityPopulationType(entity) ~= 8 then -- If players are not driving boat, we delete.
-                CancelEvent()
+        if GetEntityType(entity) == 2 then -- Check if its a vehicle
+            if GetVehicleType(entity) == "boat" then -- check if its a boat
+                if GetEntityPopulationType(entity) ~= 7 and GetEntityPopulationType(entity) ~= 8 then -- If players are not driving boat, delete
+                    CancelEvent()
+                end
             end
-
         end
-
     end)
-
 end
